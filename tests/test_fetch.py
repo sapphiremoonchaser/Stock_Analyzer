4445957f39d4c065891a8aeb763256e3777798b9
violetdiamonds <violetdiamonds@protonmail.com>
2026-01-22 16:55:34 -0500

    pytests for multiple tickers passed

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -14,18 +14,26 @@ def test_get_stock_info_single_valid_ticker():
 
     assert "error" not in info
 
-    # # Test attribute types
-    # assert isinstance(info["name"], str)
-    # assert isinstance(info["price"], (int, float, type(None)))
-    # assert isinstance(info['industry'], (str, type(None)))
-    # assert isinstance(info['sector'], (str, type(None)))
-    # assert isinstance(info['dividend_yield'], (int, float, type(None)))
-    # assert isinstance(info['trailing_pe'], (int, float, type(None)))
-    # assert isinstance(info['forward_pe'], (int, float, type(None)))
-    # assert isinstance(info['average_volume'], (int, float, type(None)))
-    # assert isinstance(info["market_cap"], (int, float, type(None)))
-    # assert isinstance(info["enterprise_value"], (int, float, type(None)))
-    # assert isinstance(info["price_to_book"], (int, float, type(None)))
+    # Test attribute types
+    assert isinstance(info["name"], str)
+    assert isinstance(info["price"], (int, float, type(None)))
+    assert isinstance(info['industry'], (str, type(None)))
+    assert isinstance(info['sector'], (str, type(None)))
+    assert isinstance(info['dividend_yield'], (int, float, type(None)))
+    assert isinstance(info['trailing_pe'], (int, float, type(None)))
+    assert isinstance(info['forward_pe'], (int, float, type(None)))
+    assert isinstance(info['average_volume'], (int, float, type(None)))
+    assert isinstance(info["market_cap"], (int, float, type(None)))
+    assert isinstance(info["enterprise_value"], (int, float, type(None)))
+    assert isinstance(info["price_to_book"], (int, float, type(None)))
 
+
+def test_get_stock_info_multiple_tickers():
+    result = get_stock_info(["MSFT", "GOOGL"])
+
+    assert len(result) == 2
+    assert "MSFT" in result
+    assert "GOOGL" in result
+    assert isinstance(result["MSFT"]['name'], str)
 
 
